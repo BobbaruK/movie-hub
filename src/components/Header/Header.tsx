e4408090d15c0dfb93e2409e6d1cefffce8de97b
Bobbaru <bobbaru_krueger@yahoo.com>
2023-11-14 13:02:12 +0200

    add darkmode switcher

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,42 @@
+import { useEffect, useState } from "react";
+import styles from "./Header.module.scss";
+
+type Theme = "dark" | "light";
+
 const Header = () => {
+  const html = document.documentElement as HTMLElement;
+
+  const [theme, setTheme] = useState<string>("dark");
+
+  useEffect(() => {
+    html.dataset.bsTheme = "dark";
+
+    return () => {};
+  }, []);
+
   return (
-    <header>
+    <header className="siteHeader">
       <div className="container">
         <div className="row">
-          <div className="col-12 my-5">
+          <div className={["col-12", styles.siteHeader__inner].join(" ")}>
             <h1>Movie hub</h1>
+            <div className="theme">
+              <button
+                className={["btn", "btn-outline-secondary"].join(" ")}
+                onClick={() => {
+                  if (theme === "light") {
+                    html.dataset.bsTheme = "dark";
+                    setTheme(() => "dark");
+
+                    return;
+                  }
+
+                  html.dataset.bsTheme = "light";
+                  setTheme(() => "light");
+                }}>
+                {theme}
+              </button>
+            </div>
           </div>
         </div>
       </div>
