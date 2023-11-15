2377e28297befe94099e2da8be6807cdf731c428
Bobbaru <bobbaru_krueger@yahoo.com>
2023-11-15 20:50:51 +0200

    reset all functionality

--- src/components/Filtering/Filtering.tsx
+++ src/components/Filtering/Filtering.tsx
@@ -1,11 +1,32 @@
+import useFilters from "../../hooks/useFilters";
 import ByGenre from "./ByGenre";
 import ByLanguage from "./ByLanguage";
 import styles from "./Filtering.module.scss";
 
 const Filtering = () => {
+  const { filters, filterBy } = useFilters();
+
   return (
     <>
-      <h2 className={[styles.filterTitle].join(" ")}>Filtering</h2>
+      <h2 className={[styles.filterTitle].join(" ")}>
+        Filtering
+        {(filters.genres.length !== 0 || filters.language) && (
+          <button
+            className="btn btn-primary btn-sm"
+            onClick={() => {
+              filterBy({
+                type: "ByGenre",
+                genreId: 0, // 0 means no genre
+              });
+              filterBy({
+                type: "ByLanguage",
+                language: "",
+              });
+            }}>
+            Reset
+          </button>
+        )}
+      </h2>
       <ByGenre />
       <ByLanguage />
     </>
