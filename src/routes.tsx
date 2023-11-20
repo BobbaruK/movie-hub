bca829878e562d9a2e3a1e6548fd99478a88db29
Bobbaru <bobbaru_krueger@yahoo.com>
2023-11-20 02:32:12 +0200

    remove unused imports

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "./App";
 import TestPage from "./pages/TestPage";
 import AppLayout from "./pages/AppLayout";
 import HomePage from "./pages/HomePage";
